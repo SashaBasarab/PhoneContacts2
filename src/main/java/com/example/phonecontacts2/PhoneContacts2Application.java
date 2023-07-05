6731a9ef0123543e86e3b94d55959b5d7794c990
SashaBasarab <oleksandr.basarab@gmail.com>
2023-07-05 22:18:58 +0300

    Initial commit

---
+++ src/main/java/com/example/phonecontacts2/PhoneContacts2Application.java
@@ -0,0 +1,13 @@
+package com.example.phonecontacts2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneContacts2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneContacts2Application.class, args);
+    }
+
+}
